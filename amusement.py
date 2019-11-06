c835aad104ebb93c6abd531be4f0f4d1d7dcb6f3
Deanna Rhodes <der113@scarletmail.rutgers.edu>
2019-11-05 23:33:13 -0500

    Update amusement.py
    I already submitted what I had, but made changes and added code hoping to get your feedback when you look over code 1.

--- amusement.py
+++ amusement.py
@@ -1,11 +1,21 @@
-#Save excel csv dataset of amusement ride accidents (2010-2017) then import it.
-#Open and read assigning it to variable amusement.
+#Save csv datatset to github project folder then import, open, read
 import csv
 source = open('Saferparks-dataset-2017.csv')
 amusement = csv.reader(source)
-#print specific columns in the dataset rows to filter only the data I need
-#(0=id, 2=state, 8=ride type, 12=number injured/killed )
-for row in amusement:
-    print row[0, 2, 8, 12]
-#create dictionary (or two) for number injured per ride type and maybe also by state
+#(0 = id, 2=state, 8=ride type, 12=number injured/killed), print dictionary to see results
+#first create empty dictionary
+ride_accidents = {}
+#skip first row in amusement because it takes the heading columns from row 1
+next(amusement)
+#iterate through rows and add specific columns to dictionary
+#when I take out id num column, it's not giving me all the states because of duplicates
+#so I added id back to make sure I had everything for each state
+for col in amusement:
+    id = col[0]
+    state = col[2]
+    ride_type = col[8]
+    num_injured = col[12]
+    ride_accidents[id] = state, ride_type, num_injured
+print ride_accidents
+#I need to total each state by ride type, then grand total
 #I would like to have a matching game for parents to teach the true risks of amusement rides
