c78fda9aa51311cf58c8ba701eadbab1d2237b4f
Deanna Rhodes <der113@scarletmail.rutgers.edu>
2019-11-08 17:42:50 -0500

    Update amusement.py
    changes

--- amusement.py
+++ amusement.py
@@ -2,20 +2,21 @@
 import csv
 source = open('Saferparks-dataset-2017.csv')
 amusement = csv.reader(source)
-#(0 = id, 2=state, 8=ride type, 12=number injured/killed), print dictionary to see results
+#(columns: 2=state, 8=ride type, 12=number injured/killed), print dictionary to see results
 #first create empty dictionary
-ride_accidents = {}
+ride_accidents_state = {}
 #skip first row in amusement because it takes the heading columns from row 1
 next(amusement)
 #iterate through rows and add specific columns to dictionary
-#when I take out id num column, it's not giving me all the states because of duplicates
-#so I added id back to make sure I had everything for each state
+#check if state isn't in dict and if not, add it in
+#create list of num injured by state
 for col in amusement:
-    id = col[0]
-    state = col[2]
-    ride_type = col[8]
-    num_injured = col[12]
-    ride_accidents[id] = state, ride_type, num_injured
-print ride_accidents
-#I need to total each state by ride type, then grand total
-#I would like to have a matching game for parents to teach the true risks of amusement rides
+    if col[2] in ride_accidents_state:
+        ride_accidents_state[col[2]].append(col[12])
+        #tried adding ride type column, but can't see a way to combine the str and int by str
+        #commented out the ride type addition
+        #ride_accidents_state[col[2]].append(col[8])
+    else:
+        ride_accidents_state[col[2]] = [col[12]] #+ [col[8]]
+
+print ride_accidents_state
